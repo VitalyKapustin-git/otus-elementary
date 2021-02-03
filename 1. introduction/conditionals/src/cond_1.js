b8c15695e326e123492307d470dd6caf6ecaf5aa
VitalyKapustin-git <workgroupkap@gmail.com>
2021-02-03 20:37:30 +0300

    Add homework for introduction module

---
+++ 1. introduction/conditionals/src/cond_1.js
@@ -0,0 +1,13 @@
+export default function printLarger (a,b) {
+	if (typeof a !== "number" || typeof b !== "number") {
+		console.log("Not a number");
+		return "Not a number";
+	}
+	if (a > b) {
+		console.log(a);
+		return a;
+	} else {
+		console.log(b);
+		return b;
+	} 
+};
---
+++ 1. introduction/conditionals/tests/cond_3.test.js
@@ -0,0 +1,13 @@
+import checkCirleInSquare from '../src/cond_3';
+
+describe('Lets check if the larger number returns', () => {
+	test('If circle = 5 in square = 2', () => {
+		expect(checkCirleInSquare(5,2)).toBe("Круг не помещается в квадрат");
+	})
+	test('If circle = 2 in square = 5', () => {
+		expect(checkCirleInSquare(2, 5)).toBe("Круг помещается в квадрат");
+	})
+	test('If return "Not a number" for {}', () => {
+		expect(checkCirleInSquare({})).toBe("Not a number");
+	})
+})
