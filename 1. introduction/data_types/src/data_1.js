6119d438dd9da427002cee1e92a0b6217296fa4b
VitalyKapustin-git <workgroupkap@gmail.com>
2021-02-03 20:37:30 +0300

    Add homework for introduction module

---
+++ 1. introduction/data_types/src/data_1.js
@@ -0,0 +1,8 @@
+export default function printMultSum(a,b) {
+	if (typeof a !== "number" || typeof b !== "number") {
+		console.log("Entered value not a number");
+		return "Entered value not a number";
+	}
+	console.log(`sum = ${a * b}, multiple = ${a + b}`);
+	return `${a * b}, ${a + b}`;
+}
---
+++ 1. introduction/data_types/tests/data_3.test.js
@@ -0,0 +1,41 @@
+import sumNumElements from '../src/data_3';
+
+// describe('Sum of elements in number', () => {
+// 	test('Expect 123 = 6', () => {
+// 		let origPrompt = window.prompt;
+// 		window.prompt = () => "321";
+// 		expect(sumNumElements()).toBe(6);
+// 		window.prompt = origPrompt;
+// 	});
+// })
+describe('Sum of elements in number', () => {
+
+	test('Return 6 when enter 123', () => {
+		// jest.spyOn - создаю обертку для метода объекта
+		// далее с помощью метода обертки mockFn.mockReturnValue подсовываю нужное значение для возврата
+		jest.spyOn(window, "prompt").mockReturnValue("123");
+		expect(sumNumElements()).toBe(6);
+	});
+	test('Return 6 when enter 321', () => {
+		jest.spyOn(window, "prompt").mockReturnValue("321");
+		expect(sumNumElements()).toBe(6);
+	});
+	test('Return "Not a number" when enter []', () => {
+		jest.spyOn(window, "prompt").mockReturnValue("[]");
+		expect(sumNumElements()).toBe("Not a number");
+	});
+	test('Return -2 when enter -512', () => {
+		jest.spyOn(window, "prompt").mockReturnValue("-512");
+		expect(sumNumElements()).toBe(-2);
+	});
+	test('Return msg about length when enter 4355', () => {
+		jest.spyOn(window, "prompt").mockReturnValue("4355");
+		expect(sumNumElements()).toBe("Length of input > 3 or < 3");
+	});
+	test('Return msg about length when enter 0', () => {
+		jest.spyOn(window, "prompt").mockReturnValue("0");
+		expect(sumNumElements()).toBe("Length of input > 3 or < 3");
+	});
+
+});
+
