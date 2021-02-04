ade7ff3fa9b36c87e7baf37dc53cb24407e21690
VitalyKapustin-git <workgroupkap@gmail.com>
2021-02-05 00:20:38 +0300

    Add id's for elements

--- 3. dom/src/index.js
+++ 3. dom/src/index.js
@@ -1,34 +1,40 @@
 import { showButton , hideButton , checkInput  } from './functions.js';
 
-const rootElement = document.querySelector('.root');
-const textAreaBlockElement = document.createElement("div");
-rootElement.append(textAreaBlockElement);
+export default function render() {
+	const rootElement = document.querySelector('.root');
+	const textAreaBlockElement = document.createElement("div");
+	textAreaBlockElement.id = 'textAreaBlockElement';
+	rootElement.append(textAreaBlockElement);
 
-for(let i = 0; i < 3; i++) {
-	let p = document.createElement("p");
-	textAreaBlockElement.append(p);
-	p.innerText = `${i}: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem molestiae numquam saepe totam explicabo in debitis magnam consectetur! Totam alias maiores natus impedit recusandae deleniti nisi porro eveniet commodi fuga.`
+	for(let i = 0; i < 3; i++) {
+		let p = document.createElement("p");
+		textAreaBlockElement.append(p);
+		p.innerText = `${i}: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem molestiae numquam saepe totam explicabo in debitis magnam consectetur! Totam alias maiores natus impedit recusandae deleniti nisi porro eveniet commodi fuga.`
+	}
+
+	let inputElement = document.createElement("input");
+	inputElement.id = "inputElement";
+	rootElement.append(inputElement);
+
+	let buttonElement = document.createElement("button");
+	buttonElement.innerText = "Add paragraph";
+	buttonElement.hidden = true;
+	buttonElement.id = "buttonElement";
+	rootElement.append(buttonElement);
+
+	inputElement.addEventListener('input', () => {
+		checkInput(inputElement, buttonElement);
+	});
+
+	buttonElement.addEventListener('click', () => {
+
+		{textAreaBlockElement.childElementCount === 5 && textAreaBlockElement.removeChild(textAreaBlockElement.childNodes[0])};
+
+		let p = document.createElement("p");
+		p.innerText = inputElement.value;
+		textAreaBlockElement.append(p);
+		inputElement.value = '';
+		hideButton(buttonElement);
+	});
 }
-
-let inputElement = document.createElement("input");
-rootElement.append(inputElement);
-
-let buttonElement = document.createElement("button");
-buttonElement.innerText = "Add paragraph";
-buttonElement.hidden = true;
-rootElement.append(buttonElement);
-
-inputElement.addEventListener('input', () => {
-	checkInput(inputElement, buttonElement);
-});
-
-buttonElement.addEventListener('click', () => {
-
-	{textAreaBlockElement.childElementCount === 5 && textAreaBlockElement.removeChild(textAreaBlockElement.childNodes[0])};
-
-	let p = document.createElement("p");
-	p.innerText = inputElement.value;
-	textAreaBlockElement.append(p);
-	inputElement.value = '';
-	hideButton(buttonElement);
-});
+render()
