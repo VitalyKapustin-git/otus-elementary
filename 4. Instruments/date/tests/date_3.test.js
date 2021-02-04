21ad6e8d8f3eb71a7e404cd9b627fdd26e04165c
VitalyKapustin-git <workgroupkap@gmail.com>
2021-02-04 23:47:43 +0300

    Add tests for instruments module

---
+++ 4. Instruments/date/tests/date_3.test.js
@@ -0,0 +1,6 @@
+import getYoungest from '../src/date_3';
+
+test('if returns the younger user correctly', () => {
+	expect(getYoungest("02.02.1970", "02.02.2002")).toBe("Second user is younger");
+	expect(getYoungest("12.08.2012", "25.08.1852")).toBe("First user is younger");
+})
--- 4. Instruments/regex/src/regex.js
+++ 4. Instruments/regex/src/regex.js
@@ -1,8 +1,8 @@
 export default function checkRegex() {
 	let userInput = prompt("Enter date(MM/DD/YY) or phone (+71234567890) or email (test@example.com)");
-	let dateRegex = new RegExp("^[0-9]{1,31}\/[0-9]{1,12}\/[0-9]{2}");
-	let phoneRegex = new RegExp("^[\+][0-9]{11}");
-	let emailRegex = new RegExp("^[a-zA-Z0-9\.]+\@[a-z]+\.[a-z]{2,5}");
+	let dateRegex = new RegExp("^[0-9]{1,31}\/[0-9]{1,12}\/[0-9]{2}$");
+	let phoneRegex = new RegExp("^[\+][0-9]{11}$");
+	let emailRegex = new RegExp("^[a-zA-Z0-9\.]+\@[a-z]+\.[a-z]{2,5}$");
 
 	if (dateRegex.test(userInput)) {
 		return "date";
