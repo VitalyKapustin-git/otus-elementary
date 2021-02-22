09b5d82497517e9596920f0eb8abfe6aa9be7fa2
VitalyKapustin-git <workgroupkap@gmail.com>
2021-02-22 18:31:54 +0300

    Add tests with spyOn(console, 'log')

--- introduction/conditionals/tests/cond_1.test.js
+++ introduction/conditionals/tests/cond_1.test.js
@@ -2,15 +2,23 @@ import printLarge from "../src/cond_1";
 
 describe("Lets check if the larger number returns", () => {
   test("If return 6 then enter 6 and 0", () => {
-    expect(printLarge(6, 0)).toBe(6);
+    jest.spyOn(console, 'log');
+    printLarge(6, 0);
+    expect(console.log).toHaveBeenCalledWith(6);
   });
   test('If return "Not a number" then enter nothing', () => {
-    expect(printLarge()).toBe("Not a number");
+    jest.spyOn(console, 'log');
+    printLarge();
+    expect(console.log).toHaveBeenCalledWith("Not a number");
   });
   test('If return "Not a number" then enter {}}', () => {
-    expect(printLarge({})).toBe("Not a number");
+    jest.spyOn(console, 'log');
+    printLarge({});
+    expect(console.log).toHaveBeenCalledWith("Not a number");
   });
   test("If return -2 then enter -10 and -3", () => {
-    expect(printLarge(-10, -3)).toBe(-3);
+    jest.spyOn(console, 'log');
+    printLarge(-10, -3)
+    expect(console.log).toHaveBeenCalledWith(-3);
   });
 });
